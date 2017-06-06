6e35e04025fa00fa830d8bb33826a1f69ba196a2
Louis Young <youngalou@gmail.com>
2017-06-06 14:23:12 -0700

    Organizing files and preparing libft.

--- libft/src/libft/count_words.c
+++ libft/src/libft/count_words.c
@@ -1,23 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   count_words.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lyoung <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/06/06 12:30:05 by lyoung            #+#    #+#             */
-/*   Updated: 2017/06/06 14:05:16 by lyoung           ###   ########.fr       */
+/*   Created: 2017/06/06 14:15:03 by lyoung            #+#    #+#             */
+/*   Updated: 2017/06/06 14:15:33 by lyoung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft/libft.h"
+#include "libft.h"
 
-int		main(int argc, char **argv)
+int		count_words(const char *s, char c)
 {
-	int		fd;
+	int		count;
+	int		in_sub;
 
-	if (argc != 2)
-		ft_printf("%{red}Invalid # of arguments.%{eoc}\nusage:\t./fdf [map]\n");
-	fd = open(argv[1], O_RDONLY);
-	return (0);
+	count = 0;
+	in_sub = 0;
+	while (*s)
+	{
+		if (*s == c && in_sub == 1)
+			in_sub = 0;
+		if (*s != c && in_sub == 0)
+		{
+			in_sub = 1;
+			count++;
+		}
+		s++;
+	}
+	return (count);
 }
