1e9d2d5d7ade36fd72f4361d8c4fa5a5f3b2b105
Louis Young <youngalou@gmail.com>
2017-06-06 13:50:41 -0700

    Just getting started!

---
+++ libft/libft.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lyoung <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/06/06 13:21:39 by lyoung            #+#    #+#             */
+/*   Updated: 2017/06/06 13:23:39 by lyoung           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include "src/libft/libft.h"
+# include "src/get_next_line/get_next_line.h"
+# include "src/ft_printf/libftprintf.h"
+
+#endif
