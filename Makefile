f5b4dcaadee31d7a5f87c1e849adbee06026b381
Louis Young <youngalou@gmail.com>
2017-06-20 15:40:45 -0700

    Finished project!

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: lyoung <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/06/07 13:21:13 by lyoung            #+#    #+#              #
-#    Updated: 2017/06/20 14:24:06 by lyoung           ###   ########.fr        #
+#    Updated: 2017/06/20 15:15:10 by lyoung           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -24,23 +24,27 @@ FILES = main.c \
 
 SRC = $(addprefix src/, $(FILES))
 
-OBJ = *.o
+OBJ = $(SRC:.c=.o)
 
 all: $(NAME)
 
-$(NAME): MLX LIBFT
-	@gcc $(CFLAG) -o $(NAME) $(SRC) $(MLX) $(LIBFT) $(MLXFLAG)
+$(NAME): $(MLX) $(LIBFT) $(OBJ)
+	@gcc $(CFLAG) -o $(NAME) $(OBJ) $(MLX) $(LIBFT) $(MLXFLAG)
 	@echo "\033[32m- fdf executable compiled\033[0m"
 
-MLX:
-	@make -C minilibx re
+$(OBJ): %.o: %.c
+	@gcc -c $(CFLAG) $< -o $@
+
+$(MLX):
+	@make -C minilibx
 	@echo "\033[32m- libmlx.a compiled\033[0m"
 
-LIBFT:
+$(LIBFT):
 	@make -C libft re
-	@make -C libft clean
 
 clean:
+	@rm -f $(OBJ)
+	@echo "\033[31m- fdf object files removed\033[0m"
 
 fclean: clean
 	@rm -f fdf
--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: lyoung <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/06 12:33:44 by lyoung            #+#    #+#             */
-/*   Updated: 2017/06/20 12:35:04 by lyoung           ###   ########.fr       */
+/*   Updated: 2017/06/20 14:27:24 by lyoung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,43 +54,43 @@ typedef struct	s_env
 ** --------------- main.c --------------
 */
 
-t_env	*init_env(void);
-void	wireframe(t_env *env);
-void	open_mlx(t_env *env, char *filename);
+t_env			*init_env(void);
+void			wireframe(t_env *env);
+void			open_mlx(t_env *env, char *filename);
 
 /*
 ** --------------- set_map.c --------------
 */
 
-void	set_map(t_env *env, int fd);
-void	populate_row(t_env *env, char *line, int y);
-void	set_scale(t_env *env);
+void			set_map(t_env *env, int fd);
+void			populate_row(t_env *env, char *line, int y);
+void			set_scale(t_env *env);
 
 /*
 ** --------------- draw_line.c --------------
 */
 
-void	open_mlx(t_env *env, char *filename);
-void	draw_segment(t_env *env, int y, int x);
-void	determine_prev(t_env *env, int y, int x);
-void	vertical_line(t_env *env, int x0, int y0);
-void	horizontal_line(t_env *env, int x0, int y0);
+void			open_mlx(t_env *env, char *filename);
+void			draw_segment(t_env *env, int y, int x);
+void			determine_prev(t_env *env, int y, int x);
+void			vertical_line(t_env *env, int x0, int y0);
+void			horizontal_line(t_env *env, int x0, int y0);
 
 /*
 ** --------------- rotate.c --------------
 */
 
-void	rotate(t_env *env, int z);
-void	rotate_hprev(t_env *env, int z);
-void	rotate_vprev(t_env *env, int z);
+void			rotate(t_env *env, int z);
+void			rotate_hprev(t_env *env, int z);
+void			rotate_vprev(t_env *env, int z);
 
 /*
 ** --------------- key_command.c --------------
 */
 
-int		key_command(int key, t_env *env);
-void	key_zoom(t_env *env, int key);
-void    key_translate(t_env *env, int key);
-void    key_color(t_env *env, int key);
+int				key_command(int key, t_env *env);
+void			key_zoom(t_env *env, int key);
+void			key_translate(t_env *env, int key);
+void			key_color(t_env *env, int key);
 
 #endif
